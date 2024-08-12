44ce4f298cb9a2de67c1d1fba93dbdf825e53375
Kiara Tesen <108550253+KiaraTesen@users.noreply.github.com>
2024-08-12 12:20:28 -0500

    Update Cargo_run_DDE - AWS

--- Cargo_run_DDE.ps1
+++ Cargo_run_DDE.ps1
@@ -4,4 +4,4 @@ $vm = [int]((hostname) -replace '\D+(\d+)','$1')
 $ip = 10+$vm
 
 #Write-Host $vm
-cargo run -- --ip "10.0.0.$ip" --port 8888 --service DE --subnet 10.0.0
+cargo run -- --ip "10.0.1.$ip" --port 8888 --service DE --subnet 10.0.1
